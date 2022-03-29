df0756b1647d167a8cd8ef74a6b351dd8fa7bf3d
Jani Palo <janipalo2004@gmail.com>
2022-03-29 12:21:28 +0300

    map 2

--- GameJam/Assets/Scenes/scripts/Loadlevel2.cs
+++ GameJam/Assets/Scenes/scripts/Loadlevel2.cs
@@ -6,6 +6,6 @@ public class Loadlevel2 : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        SceneManager.LoadScene("level2");
+        SceneManager.LoadScene(1);
     }
 }
