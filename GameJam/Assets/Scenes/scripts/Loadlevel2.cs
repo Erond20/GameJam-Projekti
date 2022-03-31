a9a1a61808780471d7c90f32a82a9fee8d79e811
Erond20 <Eronddumani04@gmail.com>
2022-03-31 13:14:35 +0300

    updatet

--- GameJam/Assets/Scenes/scripts/Loadlevel2.cs
+++ GameJam/Assets/Scenes/scripts/Loadlevel2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Loadlevel2 : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         SceneManager.LoadScene("level2");
     }
