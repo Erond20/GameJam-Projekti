bd0fb9496ff092ee4636bb81742256f5a4e12a00
Erond20 <Eronddumani04@gmail.com>
2022-04-19 11:09:52 +0300

    reload animation lisätty

--- GameJam/Assets/Script/scripts/GunProject.cs
+++ GameJam/Assets/Script/scripts/GunProject.cs
@@ -23,6 +23,7 @@ public class GunProject : MonoBehaviour
 
     public Camera fpsCam;
     public Transform attackPoint;
+    private Animator animator;
 
     public GameObject MuzzleFlash;
     public TextMeshProUGUI ammunitionDisplay;
@@ -32,6 +33,8 @@ public class GunProject : MonoBehaviour
     private void Start()
     {
             ShootAudi = GetComponent<AudioSource>();
+        animator = GetComponentInChildren<Animator>();
+
     }
     private void Awake()
     {
@@ -54,15 +57,22 @@ public class GunProject : MonoBehaviour
         else shooting = Input.GetKeyDown(KeyCode.Mouse0);
 
         if (Input.GetKeyDown(KeyCode.R) && bulletLeft < magazineSize && !reloading) reload();
+             
+
+
 
         if (readyToShoot && shooting && !reloading && bulletLeft <= 0) reload();
+     
+
         //shooting
-        if(readyToShoot && shooting && !reloading && bulletLeft >0 )
+        if (readyToShoot && shooting && !reloading && bulletLeft >0 )
         {
             //set bullet shot to 0
             BulletShot = 0;
-
+           
             Shoot();
+         
+
         }
     }
      
@@ -121,12 +131,17 @@ public class GunProject : MonoBehaviour
     {
         reloading = true;
         Invoke("ReloadFinished", reloadTime);
+        animator.PlayInFixedTime("Reload",0);
+
+
 
     }
     private void ReloadFinished()
     {
         bulletLeft = magazineSize;
         reloading = false;
+
+
     }
-    
+
 }
